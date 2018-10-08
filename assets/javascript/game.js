4035e819988d7fa5b9cb84f54753625026881864
Anthony Montalto <anthonyimontalto@gmail.com>
2018-10-08 17:17:25 -0400

    started logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// Create an array of words
+var words = [
+    "winter",
+    "throne",
+    "sword",
+    "king",
+    "dragon",
+    "fire",
+    "ice",
+    "castle"]
+
+// Create variable to store wins
+var wins=0;
+
+// Create an empty array to store guessed letters
+var guessed = [];
+
+// Create variables that hold references to the places in the HTML where we want to display things.
+// var directionsText = document.getElementById("directions-text");
+// var userChoiceText = document.getElementById("userchoice-text");
+// var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wincount");
+
+console.log(winsText);    
