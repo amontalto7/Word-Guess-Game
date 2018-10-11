6396e658a641d6ff6aaff5636cf54a84cd85176d
Anthony Montalto <anthonyimontalto@gmail.com>
2018-10-11 19:59:51 -0400

    Added logic to re-start

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+
 // Create an array of words
 var words = [
     "winter",
@@ -7,38 +8,69 @@ var words = [
     "dragon",
     "fire",
     "ice",
-    "castle"]
+    "castle"];
+
+    function populateWord(activeWord){
+        //clear arrays if there is anything inside them
+        validLetters.length = 0;
+        guessedLetters.length = 0;
+    
+        // set validLetters array - populate it with an array of letters that make up newWord
+        validLetters = activeWord.split("");   // array of letters
+        console.log ("VALIDLETTERS: "+ validLetters);
+    
+        // populate guessedLetters array with an underscore for each letter 
+        for (var i = 0; i < validLetters.length; i++){
+            guessedLetters.push("_");
+        }
+    
+        
+        // Print empty "word" to the screen
+        currentWord.textContent = guessedLetters.join("");
+        winsText.textContent = wins;
+    
+    }
+    
+
+
+
+// Create variables that hold references to the places in the HTML where we want to display things.
+var currentWord = document.getElementById("wordInput");
+var winsText = document.getElementById("winCount");
+    
+//declare Word object
+var myWord = {
+    newWord: "",
+    
+    // method to generate a new word
+    generate: function() {
+        // Get a random word and store it to the activeWord variable
+        this.newWord = words[Math.floor(Math.random() * words.length)];
+        return this.newWord;
+    }   
+ 
+}
 
 // Create variable to store wins
 var wins=0;
 
 // Create an empty array to store guessed letters
-var guessed = [];
-
-// Create variables that hold references to the places in the HTML where we want to display things.
-var currentWord = document.getElementById("wordInput");
-var winsText = document.getElementById("winCount");
-
-var activeWord = words[Math.floor(Math.random() * words.length)];
 var guessedLetters = [];
-var validLetters = activeWord.split("");   // array of letters
+var validLetters = [];
 var correctGuesses = 0;
 var incorrectGuesses = 0;
 
+var activeWord = myWord.generate();
+    console.log ("ACTIVEWORD: "+ activeWord);
 
-for (var i = 0; i < validLetters.length; i++){
-    guessedLetters.push("_");
-}
+populateWord(activeWord);
 
-// Print empty "word" to the screen
-currentWord.textContent = guessedLetters.join("");
-winsText.textContent = wins;
+
+console.log("word: " +activeWord);
 
 
 console.log("Length of word: " + activeWord.length);
 console.log("Correct Guesses: "+correctGuesses);
-console.log("Active word: " + activeWord);
-console.log("letters: "+ validLetters);
 console.log('Before: ', winsText);
 
 // winsText.textContent = "Testing";
@@ -54,16 +86,17 @@ console.log('Before: ', winsText);
         console.log();
         console.log(activeWord);
 
-        if (activeWord.includes(userGuess)){
+        if ((activeWord).includes(userGuess)){
             correctGuesses++;            
-            guessedLetters[validLetters.indexOf(userGuess)] = userGuess;        
+            guessedLetters[(validLetters).indexOf(userGuess)] = userGuess;        
             currentWord.textContent = guessedLetters.join("");
             
-            if (correctGuesses === validLetters.length){
+            if (correctGuesses === (validLetters).length){
                 wins++;
                 winsText.textContent = wins;
                 correctGuesses = 0;
-                
+                activeWord = myWord.generate();
+                populateWord(activeWord);
             }
 
         } else {
